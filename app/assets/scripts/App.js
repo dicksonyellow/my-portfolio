af7cc5f9d8dead7bf347c346eba1bb41ceaf416e
Dickson <esiebadickson89@gmail.com>
2020-12-15 15:37:54 +0100

    completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import '../styles/styles.css'
+import RevealOnScroll from './modules/Reveal-on-scroll'
+
+new RevealOnScroll(document.querySelectorAll('.skills'), 70)
+new RevealOnScroll(document.querySelectorAll('.recent-projects__project-item'), 60)
 
 if (module.hot) {
     module.hot.accept()
